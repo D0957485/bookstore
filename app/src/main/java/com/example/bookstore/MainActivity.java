621397c6ef1d0bca74fca234e188f589730d1806
tuquancheng <D0989354@o365.fcu.edu.tw>
2022-05-23 23:11:31 +0800

    Test push 0523 Tu

--- app/src/main/java/com/example/bookstore/MainActivity.java
+++ app/src/main/java/com/example/bookstore/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
   //hiii test
-  //
+  //tututu
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
