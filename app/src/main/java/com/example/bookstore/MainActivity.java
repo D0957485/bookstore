9c3f042a3cc2235d6b26bc58b1ccb1dfaf7d84cd
Kireiun <cystds@gmail.com>
2022-05-23 23:19:03 +0800

    0523_Yisin_TEST

--- app/src/main/java/com/example/bookstore/MainActivity.java
+++ app/src/main/java/com/example/bookstore/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
   // ooooooooooooooooOoOoOoOo Lu
   //KKKKK
   //KKKKKKKKK
-
+  //yamete >_<
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
